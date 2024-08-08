5a3d26baddf21e400a6932aaf4e58fafe3d7ed4c
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-08-08 21:38:36 +0300

    feat: new gamemanager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float waves = 5f;
+    void Start()
+    {
+        SpawnWave();
+    }
+
+    void SpawnWave() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
