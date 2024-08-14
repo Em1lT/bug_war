9a1029a444ae4ec28532b185919a1167fdf9f6a4
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-08-14 22:35:17 +0300

    feat: more flowers & grenade tag

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Flower : MonoBehaviour
 {
     // Start is called before the first frame update
+    private SpriteRenderer image;
+    public Sprite variant1;
+    public Sprite variant2;
+    public Sprite variant3;
+    public Sprite variant4;
     private Animator animator;
     private AnimationStates currentState;
 
     private enum AnimationStates {
         flower_idle,
         flower_step,
+        flower_wave
     }
 
     void ChangeAnimationState(AnimationStates newState) {
@@ -26,13 +32,47 @@ public class Flower : MonoBehaviour
         animator.enabled = true;
     }
 
+    private IEnumerator FlowerWave() {
+        animator.enabled = true;
+        ChangeAnimationState(AnimationStates.flower_wave);
+        yield return new WaitForSeconds(1f);
+        animator.enabled = false;
+    }
+
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
+        animator.enabled = false;
         // random float
         float random = Random.Range(0.5f, 2.5f);
-        Debug.Log(random);
-        StartCoroutine(PlayFlower(random));
+        int randomVariant = Random.Range(0, 4);
+        int randomRotation = Random.Range(1, 3);
+        Debug.Log(randomRotation);
+        switch(randomRotation) {
+            case 1:
+                transform.Rotate(0f, 180f, 0f);
+                break;
+            case 2:
+                transform.Rotate(0f, 0f, 0f);
+                break;
+        }   
+
+        image = gameObject.GetComponent<SpriteRenderer>();
+        switch(randomVariant) {
+            case 0:
+                image.sprite = variant1;
+                break;
+            case 1:
+                image.sprite = variant2;
+                break;
+            case 2:
+                image.sprite = variant3;
+                break;
+            case 3:
+                image.sprite = variant4;
+                break;
+        }
+        // StartCoroutine(PlayFlower(random));
     }
 
     // Update is called once per frame
@@ -41,14 +81,19 @@ public class Flower : MonoBehaviour
         
     }
     private IEnumerator FlowerStep() {
+        animator.enabled = true;
         ChangeAnimationState(AnimationStates.flower_step);
         yield return new WaitForSeconds(0.5f);
         ChangeAnimationState(AnimationStates.flower_idle);
+        animator.enabled = false;
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player") || other.CompareTag("Enemy")) {
             StartCoroutine(FlowerStep());
+        }
+        if(other.CompareTag("Grenade")) {
+            StartCoroutine(FlowerWave());
         }
     }
 }
